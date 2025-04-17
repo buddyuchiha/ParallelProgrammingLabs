5b9db235072139f69df14a6d6c8aad0e07446ff3
Buddy <Pro100biv@mail.ru>
2025-04-17 17:07:07 +0400

    multy matrix realization + tests

--- ParallelProgrammingLab1/ParallelProgrammingLab1.cpp
+++ ParallelProgrammingLab1/ParallelProgrammingLab1.cpp
@@ -18,6 +18,26 @@ vector<vector<int>> CreateMatrix() {
     return matrix;
 }
 
+vector<vector<int>> MultyMatrx(vector<vector<int>> first_matrix, vector<vector<int>> second_matrix) {
+    int first_columns = first_matrix.size();
+    int second_columns = second_matrix.size();
+    int first_rows = first_matrix[0].size();
+    int second_rows = second_matrix[0].size();
+    if (first_columns != second_rows) {
+        throw "EXCEPTION: first_columns != second_rows";
+    }  
+    vector<vector<int>> result_matrix(second_rows, vector<int>(first_columns));
+    for (int i = 0; i < first_rows; i++) {
+        for (int j = 0; j < second_columns; j++) {
+            result_matrix[i][j] = 0;
+            for (int k = 0 ; k < first_rows; k++) {
+                result_matrix[i][j] += first_matrix[i][k] * second_matrix[k][j];
+            }
+        }
+    }
+    return result_matrix;
+}
+
 void PrintMatrix(vector<vector<int>> matrix) {
     int rows = matrix.size();
     int columns = matrix[0].size();
@@ -33,6 +53,10 @@ int main()
 {
     vector<vector<int>> first_matrix = CreateMatrix();
     PrintMatrix(first_matrix);
+    vector<vector<int>> second_matrix = CreateMatrix();
+    PrintMatrix(second_matrix);
+    vector<vector<int>> result_matrix = MultyMatrx(first_matrix, second_matrix);
+    PrintMatrix(result_matrix);
 
 }
 
